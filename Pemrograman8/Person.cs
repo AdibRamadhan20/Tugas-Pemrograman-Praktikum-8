d052d352c99fc153151c29053f911f1ff4e7b6c8
AdibRamadhan20 <adib2003@students.amikom.ac.id>
2023-06-06 03:12:11 +0700

    first commit

---
+++ Pemrograman8/Person.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pemrograman8
+{
+	// Class Person yaitu sebagai Class Induk
+	public class Person
+	{
+		// Property Class Person
+		public string Name;
+		public int Age;
+
+		// Constructor Class Person
+		public Person(string name, int age)
+		{
+			this.Name = name;
+			this.Age = age;
+		}
+
+		// Method atau Fungsi yang menampilkan nama dan umur
+		public void InfoPerson()
+		{
+			Console.WriteLine("Nama saya {0}, Umur saya {1} tahun", Name, Age);
+		}
+
+
+	}
+
+	// Class Karyawan yaitu sebagai Class Turunan dari Class Person
+	public class Karyawan : Person
+	{
+		// Property Class Karyawan
+		public string KaryawanID;
+		public string Subject;
+
+		//Constructor Class Karyawan
+		public Karyawan(string karyawanID, string subject, string name, int age) : base(name, age)
+		{
+			KaryawanID = karyawanID;
+		    Subject = subject;
+		}
+
+		// Method Class Karyawan
+		public void InfoKaryawan()
+		{
+			Console.Write("Saya berposisi sebagai {0} dengan ID {1}", Subject, KaryawanID);
+		}
+
+		//Override Method dari Class Person
+		public void InfoPerson(string name, int age)
+		{
+			Console.WriteLine("Nama saya {0}, umur saya {1} tahun", Name, Age);
+		}
+	}
+}
---
+++ Pemrograman8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pemrograman8
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("=======================");
+			Console.WriteLine("||    Class Induk    ||");
+			Console.WriteLine("=======================");
+			// Object Class Person dengan menggunakan Constructor
+			Person person1 = new Person("Adib Ramadhan", 29);
+			// Memanggil Method Class Person
+			person1.InfoPerson();
+
+			Console.WriteLine("\n=======================");
+			Console.WriteLine("||   Class Turunan   ||");
+			Console.WriteLine("=======================");
+			// Object Class Karyawan dengan menggunakan Constructor
+			Karyawan karyawan1 = new Karyawan("23901117", "Direktur Keuangan", "Rizki Anandra", 32);
+			// Memanggil Method Override dari Class Person ke Class Karyawan
+			karyawan1.InfoPerson();
+			// Memanggil Method Class Karyawan
+			karyawan1.InfoKaryawan();
+			Console.ReadKey();
+
+		}
+	}
+}
+
